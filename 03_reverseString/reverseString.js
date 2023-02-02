c5db0fc7adbbcbc94c692af3e8e426739c986d18
Andrew Akers <andrewpakers@gmail.com>
2023-02-02 03:44:45 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(targetStr) {
+    let reverseArray = [];
+    let returnStr = "";
+    for (let i = 0; i < targetStr.length; i++){
+        reverseArray.push(targetStr[(targetStr.length - 1) - i]);
+    }
+    returnStr = reverseArray.join("");
+    return returnStr;
 };
 
 // Do not edit below this line
