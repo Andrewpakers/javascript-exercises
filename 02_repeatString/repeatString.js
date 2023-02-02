8faba071ff015c8cdd8875725970aa496a8bd93a
Andrew Akers <andrewpakers@gmail.com>
2023-02-02 03:33:07 -0500

    Complete exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(targetStr, iterations) {
+    let returnStr = "";
+    for (let i = 0; i < iterations; i++){
+        returnStr += targetStr;
+    }
+    if (iterations < 0) {
+        returnStr = "ERROR";
+    }
+    return returnStr;
 };
 
 // Do not edit below this line
