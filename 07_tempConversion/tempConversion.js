3c60259b63c4c65329e95fedc05aee38e993c815
Andrew Akers <andrewpakers@gmail.com>
2023-02-03 12:32:24 -0500

    Complete exercises 7 and 8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(targetTemp) {
+  let result = parseFloat(((targetTemp -32) * (5/9)).toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(targetTemp) {
+  let result = parseFloat((targetTemp * (9/5) + 32).toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(arg1, arg2) {
+	return arg1 + arg2;
+};
+
+const subtract = function(arg1, arg2) {
+  return arg1 - arg2;
+};
+
+const sum = function(arg1) {
+	let sum = 0;
+  for (let i = 0; i < arg1.length; i++) {
+    sum += arg1[i];
+  }
+  return sum;
+};
+
+const multiply = function(arg1) {
+  let result = 1;
+  for (let i = 0; i < arg1.length; i++){
+    result *= arg1[i];
+  }
+  return result;
+};
+
+const power = function(arg1, arg2) {
+  return arg1 ** arg2;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(arg1) {
+	let result = 1;
+  if (arg1 == 0 || arg1 == 1){
+    return 1;
+  } else {
+    for (let i = arg1; i >= 1; i--) {
+      result = result * i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
