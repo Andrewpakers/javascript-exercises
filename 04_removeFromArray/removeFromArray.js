0a7077822e14f47fe896e3bd507a6cfb69eecca4
Andrew Akers <andrewpakers@gmail.com>
2023-02-03 10:54:10 -0500

    Complete exercises 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(targetArray) {
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < targetArray.length; j++) {
+            if (targetArray[j] === arguments[i]) {
+                targetArray.splice(j, 1);
+            }
+        }
+    }
+    return targetArray;
+
+
 
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    let sum = 0;
+    let rangeBottom, rangeTop;
+    if (arg1 < arg2) {
+        rangeBottom = arg1;
+        rangeTop = arg2;
+    } else {
+        rangeBottom = arg2;
+        rangeTop = arg1;
+    }
+    if (rangeBottom < 0 || rangeTop < 0) {
+        return "ERROR";
+    } else if (typeof rangeBottom != "number" || typeof rangeTop != "number") {
+        return "ERROR";
+    }
+    for (let i = rangeBottom; i <= rangeTop; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
