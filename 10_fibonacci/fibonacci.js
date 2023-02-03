b90cd50099ca97677df8150029084cc5961a9cd5
Andrew Akers <andrewpakers@gmail.com>
2023-02-03 16:57:45 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(arg1) {
+    const fibSec = [1,1];
+    if (arg1 < 1) {
+        return "OOPS";
+    }
+    for (let i = 2; i < arg1; i++) {
+        fibSec.push(fibSec[i-2]+fibSec[i-1]);
+    }
+    console.log(fibSec);
+    return fibSec[arg1-1];
 };
 
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
