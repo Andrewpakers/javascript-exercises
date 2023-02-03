fc41c2202ee79e8232f2692705a05f046e36e0b6
Andrew Akers <andrewpakers@gmail.com>
2023-02-03 18:02:22 -0500

    Complete exercise 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (let i = 0; i < people.length; i++) {
+        if (typeof people[i].yearOfDeath === 'undefined') {
+            people[i].age = (new Date().getFullYear()) - people[i].yearOfBirth;            
+        } else {
+            people[i].age = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+    }
+    people.sort((a, b) => (a.age < b.age) ? 1 : -1)
+    return people[0];
 };
 
 // Do not edit below this line
